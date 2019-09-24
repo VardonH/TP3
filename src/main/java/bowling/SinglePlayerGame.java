aa6ca0c845421eb731e3d2e62217a690f4c03a2a
pedago <pedago@pcalb-mm0607.univ-jfc.fr>
2019-09-24 15:06:02 +0200

    TP3_first_save

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -1,5 +1,7 @@
 package bowling;
 
+import java.util.ArrayList;
+
 /**
  * Cette classe a pour but d'enregistrer le nombre de quilles abattues lors des
  * lancés successifs d'<b>un seul et même</b> joueur, et de calculer le score
@@ -7,10 +9,14 @@ package bowling;
  */
 public class SinglePlayerGame {
 
+        public ArrayList score;
+        public int tours;
 	/**
 	 * Constructeur
 	 */
 	public SinglePlayerGame() {
+            tours = 20;
+            score = new ArrayList<Integer>();
 	}
 
 	/**
@@ -20,8 +26,55 @@ public class SinglePlayerGame {
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
-
+            
+            //ajout de deux boules si stike et dernier tour
+            if (nombreDeQuillesAbattues==10) {
+                tours-=1;  
+                if ( tours == 1) {
+                    tours+=2;
+                }
+            }    
+                
+            tours -= 1;
+            this.score.add(nombreDeQuillesAbattues);                 
+            
 	}
+        
+        public void spare() {
+            
+        }
+        
+        /**
+         * Verifie si il y a un strike dans l'arrayList score et si il y en a
+         * ajoute le score des deux lancés le suivant
+         * @param sc l'arrayList score
+         */
+        public void strike(ArrayList<Integer> sc) {
+            int newValue = 0;  
+            
+            for (int i = 2; i<sc.size(); i++) {
+                if (sc.get(i-2)==10) {
+                    newValue = (sc.get(i) + (sc.get(i)-1));
+                    sc.set(i-2,newValue);
+                }
+            }
+        }
+        
+        /**
+         * 
+         * @param sc une arraylist score
+         * @return le score total de type int issue de l'arraylist
+         */
+        public int total(ArrayList<Integer> sc) {
+            int total = 0;
+            
+            strike(score);
+            
+            for(int i=0; i<sc.size();i++) {
+                total += sc.get(i);
+            }
+            return total;
+        }
 
 	/**
 	 * Cette méthode donne le score du joueur
@@ -29,6 +82,7 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            return total(this.score);
+               
 	}
 }
